d9f201fdd1154f9bb57ea0816090d3d3397e4bcf
Manya <gulatimanya16@gmail.com>
2024-12-05 15:05:06 +0530

    added dotenv

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
+
+import env from "dotenv";
+
+
+env.config();
 export const connect = ()=>{
-    const mongoDB=process.env.MONGO_URL;
+    const mongoDB = process.env.MONGO_URL;
     mongoose.connect(mongoDB,(err)=>{
         if(err){
             console.log("ERROR : UNABLE TO CONNECT");
--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ const app = express();
 connect();
 
 // Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
